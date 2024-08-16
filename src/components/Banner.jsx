8815f6eb489623eb8c866faf495fa04de49ed084
Laudkyle <kyleaby1@gmail.com>
2024-08-16 09:56:08 +0100

    md changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,15 +6,15 @@ import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section className="min-h-[85vh] lg:min-h-[78vh] flex items-center" id="home">
+    <section className="min-h-[85vh] md:min-h-[78vh] flex items-center" id="home">
       <div className="container mx-auto">
-        <div className="flex flex-col gap-y-8 lg:flex-row lg:gap-x-12 lg:items-center">
-          <div className="flex-1 text-center font-secondary lg:text-left">
-            <h1 className="text-[55px] leading-[0.8] font-bold lg:text-[110px] ">
+        <div className="flex flex-col gap-y-8 md:flex-row md:gap-x-12 md:items-center">
+          <div className="flex-1 text-center font-secondary md:text-left">
+            <h1 className="text-[55px] leading-[0.8] font-bold md:text-[110px] ">
               Ben
               <span> AYDEN</span>
             </h1>
-            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+            <div className="mb-6 text-[36px] md:text-[60px] font-secondary font-semibold uppercase leading-[1]">
               <span className="mr-4 text-white">I am a</span>
               <TypeAnimation
                 sequence={[
@@ -32,13 +32,13 @@ const Banner = () => {
               />
             </div>
 
-            <p className="mb-8 max-w-lg mx-auto lg:mx-0">
+            <p className="mb-8 max-w-lg mx-auto md:mx-0">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
               dolores dolore consequatur animi neque sapiente distinctio ratione
               sint odit cum.
             </p>
 
-            <div className="flex max-w-max gap-x-6 items-center mx-auto mb-16 lg:mx-0">
+            <div className="flex max-w-max gap-x-6 items-center mx-auto mb-16 md:mx-0">
               <button className="capitalize btn btn-lg">Contact me</button>
               <a href="#" className="text-gradient btn-link">
                 My portfolio
